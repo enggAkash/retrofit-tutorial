da920c466abe3d486e80aef6328a9b7efaeb8e0c
Akash Kumar <mynameisakashkumar@outlook.com>
2019-01-04 08:45:55 +0530

    further working on file upload through retrofit

--- app/src/main/java/com/example/retrofitrough/api/service/UserClient.java
+++ app/src/main/java/com/example/retrofitrough/api/service/UserClient.java
@@ -2,13 +2,25 @@ package com.example.retrofitrough.api.service;
 
 import com.example.retrofitrough.api.model.User;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface UserClient {
 
-    @POST("/api/users")
+    @POST("api/users")
     Call<User> createUser(@Body User user);
 
+    @Multipart
+    @POST("api/users")
+    Call<ResponseBody> createUser(
+            @Part User user,
+            @Part("description") RequestBody description,
+            @Part MultipartBody.Part avatar
+    );
 }
--- app/src/main/java/com/example/retrofitrough/ui/PostActivity.java
+++ app/src/main/java/com/example/retrofitrough/ui/PostActivity.java
@@ -1,5 +1,14 @@
 package com.example.retrofitrough.ui;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PermissionGroupInfo;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +21,8 @@ import com.example.retrofitrough.R;
 import com.example.retrofitrough.api.model.User;
 import com.example.retrofitrough.api.service.UserClient;
 
+import java.security.Permission;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
@@ -22,9 +33,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PostActivity extends AppCompatActivity {
 
+    private static final int READ_EXTERNAL_STORAGE_REQUEST_CODE = 1;
+    private static final int PICK_IMAGE_FROM_GALLERY = 2;
     private EditText nameEt;
     private EditText jobEt;
+    private Button uploadAvtar;
     private Button createAccountBtn;
+    private Uri avtarUri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +49,18 @@ public class PostActivity extends AppCompatActivity {
         nameEt = findViewById(R.id.name_et);
         jobEt = findViewById(R.id.job_et);
         createAccountBtn = findViewById(R.id.create_account_btn);
+        uploadAvtar = findViewById(R.id.upload_avtar);
+
+        uploadAvtar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(intent, "Choose Avtar"), PICK_IMAGE_FROM_GALLERY);
+            }
+        });
 
         createAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +71,17 @@ public class PostActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    READ_EXTERNAL_STORAGE_REQUEST_CODE);
     }
 
     private void sendNetworkRequest(User user) {
@@ -87,5 +125,33 @@ public class PostActivity extends AppCompatActivity {
 
     }
 
+    private void uploadFile(Uri fileUri) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+
+            case READ_EXTERNAL_STORAGE_REQUEST_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted
+                }
+            }
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_FROM_GALLERY && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            uploadFile(uri);
+        }
+    }
 }
